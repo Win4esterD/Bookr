25e1c649ebc30e545e2b3351e4238c3dd0ad4186
Pavel Didenko <win4ester2016@yandex.ru>
2022-04-19 21:16:11 +0300

    Подключил БД и создал несколько вьюшек с темплейтами.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from reviews.models import Publisher, Contributor, Book, BookContributor, Review
+from django.contrib.admin import ModelAdmin
+ # Register your models here.
 
-# Register your models here.
+class BookAdmin(ModelAdmin):
+    date_hierarchy = 'publication_date'
+    list_display = ('title', 'isbn')
+    list_filter = ('publisher', 'publication_date', )
+    search_fields = ('title', 'isbn')
+
+class ReviewAdmin(ModelAdmin):
+    fieldsets = (('Linkage', {'fields': ('creator', 'book')}), ('Review content', {'fields': ('content', 'rating')}))
+
+class ContributorAdmin(ModelAdmin):
+    list_display = ('last_names', 'first_names')
+    search_fields = ('last_names__startswith', 'first_names')
+    list_filter = ('last_names', )
+
+admin.site.register(Publisher)
+admin.site.register(Contributor, ContributorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookContributor)
+admin.site.register(Review, ReviewAdmin)
+
+
+
